2d34477e1d8f918daba2f39ed698c65ec9cea4ad
Lucas Licurgo <lucasclicurgo@gmail.com>
2024-04-27 18:28:37 -0300

    Realização da parte backend

--- backend/src/rotas.js
+++ backend/src/rotas.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const usuarios = require('./controlador/usuarios');
 
 const rotas = express();
 
 rotas.get('/', (req, res) => {
     return res.send('Servidor ligado.')
-})
+});
+
+rotas.post('/usuarios', usuarios.cadastrarUsuario);
+rotas.post('/login', usuarios.login);
 
 module.exports = rotas;
